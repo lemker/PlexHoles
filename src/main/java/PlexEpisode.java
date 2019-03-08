fe016b51a68ae23c9293cc09008780df8882b15f
Lehel <lehelmk@gmail.com>
2019-03-07 23:51:49 -0800

    Created PlexEpisode.java

---
+++ src/main/java/PlexEpisode.java
@@ -0,0 +1,30 @@
+class PlexEpisode {
+    private String title;
+    private int parentIndex;
+    private int index;
+
+    String getTitle() {
+        return title;
+    }
+
+    void setTitle(String input) {
+        title = input;
+    }
+
+    int getParentIndex() {
+        return parentIndex;
+    }
+
+    void setParentIndex(int input) {
+        parentIndex = input;
+    }
+
+    int getIndex() {
+        return index;
+    }
+
+    void setIndex(int input) {
+        index = input;
+    }
+
+}
